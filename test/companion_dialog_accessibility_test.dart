9d7ea91b8d2e0460e344c767eab20f7e224fc464
Sterre <48180370+sterre-g@users.noreply.github.com>
2025-05-28 22:13:45 +0000

    Add accessibility test for companion_dialog

---
+++ test/companion_dialog_accessibility_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:testing/AccessibilityTest.dart';
+
+class CompanionDialogAccess_T /
+
+  const Builder builder = com of(
+    <WindowContext width=100, height=100>
+      <CompanionBuilder(
+        isTypical: yes,
+        isSpecial: no,
+          parentProviderKey: "companion_dialog_access"
+      ) />
+    </WindowContext>
+  />
+).pr();
+
+if ( ongoing) builder.start();
