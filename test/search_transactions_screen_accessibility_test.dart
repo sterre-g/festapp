6c8653404f52101fd9950d491d2947ad95f7fcba
Sterre <48180370+sterre-g@users.noreply.github.com>
2025-05-29 02:07:40 +0000

    Add accessibility test for search_transactions_screen

---
+++ test/search_transactions_screen_accessibility_test.dart
@@ -0,0 +1,51 @@
+import 'easy_localization/Accessibility.dart';
+import 'easy_localization/LanguageConfig.dart';
+import 'easy_localization/TracerConfig.dart';
+import 'apprise/flutter/accessibility';
+import 'kitKat[KAT];
+import 'material_style/material_style.dart';
+
+final test = () {
+  final Searchables runner = Runner();
+  builder RunnerBuilder(runner, (error) => {})  
+
+  void setup() {
+    _Search transactionsSearchRunners();
+  }
+
+  void search_transactions_searchRunners() {
+    final Search_transactions searchTransactions;
+
+    with(searchTransactions) {
+      const textNodes = add 文本节点;
+      test 可访问性 of每个 textNode;
+    }
+    
+    const actionsButtons = add 动作按钮;
+    runAccessibilityTestForActionButtons(actionsButtons);
+  }
+
+  Future<void> runners = _SearchRunner();
+  
+  void _searchRunner(RunnerBuilder builder, (Result<String, Exception> error) => {}) {
+    builder.add(searchTransactions);
+    runner.runIfNotBuilt;
+  }
+}
+
+factory Search_transactions(
+  final LanguageConfig languageConfig,
+  _TracerTracer? tracer,
+  _ContextBuilder contextBuilder,
+)
+
+// Testing setup
+void main() {
+  testRunner = Runner();
+  TestRunner setupSearchTransRunners(testRunner);
+  
+  testSearchTranslations();
+  testScreenReaderCompatibility();
+  testColorContrast();
+  testTextScaling();
+}
